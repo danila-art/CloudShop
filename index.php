f5378ffca3b1de7822f43a13e587683e0f89eeca
danila-art <danila.sivov.96@mail.ru>
2023-07-29 18:46:15 +0400

    first commit

---
+++ index.php
@@ -0,0 +1,70 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cloud Shop -- Главная</title>
+    <link rel="stylesheet" href="assets/css/main_stylesheet.css">
+</head>
+
+<body>
+    <?php require_once './modules/header.php'; ?>
+    <div class="container">
+        <div class="container__item">
+            <img src="" alt="" srcset="">
+            <h4 class="item__title">Название товара</h4>
+            <div class="container__item-rating"><span>4.5⭐</span></div>
+            <div class="container__item-desk">
+                <span>Lorem ipsum dolor sit, amet consectetur adipisicing elit. At quas qui illo eveniet ducimus aut.</span>
+            </div>
+            <a href="#">Подробнее</a>
+
+        </div>
+        <div class="container__item">
+            <img src="" alt="" srcset="">
+            <h4 class="item__title">Название товара</h4>
+            <div class="container__item-rating"><span>4.5⭐</span></div>
+            <div class="container__item-desk">
+                <span>Lorem ipsum dolor sit, amet consectetur adipisicing elit. At quas qui illo eveniet ducimus aut.</span>
+            </div>
+            <a href="#">Подробнее</a>
+
+        </div>
+        <div class="container__item">
+            <img src="" alt="" srcset="">
+            <h4 class="item__title">Название товара</h4>
+            <div class="container__item-rating"><span>4.5⭐</span></div>
+            <div class="container__item-desk">
+                <span>Lorem ipsum dolor sit, amet consectetur adipisicing elit. At quas qui illo eveniet ducimus aut.</span>
+            </div>
+            <a href="#">Подробнее</a>
+
+        </div>
+        <div class="container__item">
+            <img src="" alt="" srcset="">
+            <h4 class="item__title">Название товара</h4>
+            <div class="container__item-rating"><span>4.5⭐</span></div>
+            <div class="container__item-desk">
+                <span>Lorem ipsum dolor sit, amet consectetur adipisicing elit. At quas qui illo eveniet ducimus aut.</span>
+            </div>
+            <a href="#">Подробнее</a>
+
+        </div>
+        <div class="container__item">
+            <img src="" alt="" srcset="">
+            <h4 class="item__title">Название товара</h4>
+            <div class="container__item-rating"><span>4.5⭐</span></div>
+            <div class="container__item-desk">
+                <span>Lorem ipsum dolor sit, amet consectetur adipisicing elit. At quas qui illo eveniet ducimus aut.</span>
+            </div>
+            <a href="#">Подробнее</a>
+
+        </div>
+    </div>
+</body>
+
+</html>
